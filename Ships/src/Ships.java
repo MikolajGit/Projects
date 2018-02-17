eb92018d8d8697cf24d2c83a84f2b8a964b1c637
DESKTOP-HSF813D\mikig <mikigrz@op.pl>
2018-02-17 22:54:58 +0100

    Po usunieciu/Od nowa

--- Ships/src/Ships.java
+++ Ships/src/Ships.java
@@ -7,21 +7,13 @@ public class Ships {
         Playar playar1 = new Playar();
         Playar playar2 = new Playar();
 
-        ShipVariables field = new ShipVariables();
-/*
-        field.setShips(1,1);
-        System.out.println(field.getShips(1,1));
-        System.out.println(field.getShips(1,2));
-
         System.out.print("Give the name of the first player: ");
         playar1.setName(data.next());
         System.out.print("Give the name of the second player: ");
         playar2.setName(data.next());
 
         System.out.println(playar1.getName());
-        System.out.println(playar2.getName());*/
-
-        field.setVarX("a");
-        System.out.println(field.getVarX());
+        System.out.println(playar2.getName());
+        
     }
 }
