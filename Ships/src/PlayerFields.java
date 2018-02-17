4fe6f250920402e6f041156262a253048cb6e21d
DESKTOP-HSF813D\mikig <mikigrz@op.pl>
2018-02-17 23:42:28 +0100

    Poka pierwszego gracza - ustalanie

---
+++ Ships/src/PlayerFields.java
@@ -0,0 +1,47 @@
+public class PlayerFields {
+  private String ship1;
+  private String ship2;
+  private String ship3;
+  private String ship4;
+  private String ship5;
+
+    public void setShip1(String ship1) {
+        this.ship1 = ship1;
+    }
+
+    public void setShip2(String ship2) {
+        this.ship2 = ship2;
+    }
+
+    public void setShip3(String ship3) {
+        this.ship3 = ship3;
+    }
+
+    public void setShip4(String ship4) {
+        this.ship4 = ship4;
+    }
+
+    public void setShip5(String ship5) {
+        this.ship5 = ship5;
+    }
+
+    public String getShip1() {
+        return ship1;
+    }
+
+    public String getShip2() {
+        return ship2;
+    }
+
+    public String getShip3() {
+        return ship3;
+    }
+
+    public String getShip4() {
+        return ship4;
+    }
+
+    public String getShip5() {
+        return ship5;
+    }
+}
--- Ships/src/Ships.java
+++ Ships/src/Ships.java
@@ -7,13 +7,33 @@ public class Ships {
         Playar playar1 = new Playar();
         Playar playar2 = new Playar();
 
+        PlayerFields fieldsPlayer1 = new PlayerFields();
+        PlayerFields fieldsPlayer2 = new PlayerFields();
+
         System.out.print("Give the name of the first player: ");
         playar1.setName(data.next());
         System.out.print("Give the name of the second player: ");
         playar2.setName(data.next());
 
-        System.out.println(playar1.getName());
-        System.out.println(playar2.getName());
-        
+        System.out.println("Let's get started!");
+        System.out.println("Player: " + playar1.getName());
+        System.out.print("Enter first field: ");
+        fieldsPlayer1.setShip1(data.next());
+        System.out.print("Enter second field: ");
+        fieldsPlayer1.setShip2(data.next());
+        System.out.print("Enter third field: ");
+        fieldsPlayer1.setShip3(data.next());
+        System.out.print("Enter fourth field: ");
+        fieldsPlayer1.setShip4(data.next());
+        System.out.print("Enter fifth field: ");
+        fieldsPlayer1.setShip5(data.next());
+
+        System.out.println(fieldsPlayer1.getShip1());
+        System.out.println(fieldsPlayer1.getShip2());
+        System.out.println(fieldsPlayer1.getShip3());
+        System.out.println(fieldsPlayer1.getShip4());
+        System.out.println(fieldsPlayer1.getShip5());
+
+
     }
 }
